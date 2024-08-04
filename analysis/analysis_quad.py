413b57a958bd1996055164eb1ceeaa720539b151
jiang4 <jiang4@jrlogin09.jureca>
2024-08-04 15:09:21 +0200

    Simplify analysis_quad.py

--- analysis/analysis_quad.py
+++ analysis/analysis_quad.py
@@ -11,58 +11,31 @@ import matplotlib.pyplot as plt
 plt.rcParams.update({'font.size': 18})
 
 # Load analysis configurations
-with open('../config/analysis_config.yaml') as analysis_config_file:
+config_file_name = '../config/analysis_config.yaml'
+with open(config_file_name) as analysis_config_file:
     config = yaml.load(analysis_config_file, Loader=yaml.FullLoader)
 
 # Load data
-# first
-jube_id_1 = str(sys.argv[1])
-path_1 = os.path.join(config['jube_outpath'], jube_id_1.zfill(6))
-timer_file_1=os.path.join(path_1, "timer_file.csv")
-# analyse and save plot raw data to .csv
-shell(
-    f"module load JUBE; jube analyse {config['jube_outpath']} --id {jube_id_1};"
-    + f" jube result {config['jube_outpath']} --id {jube_id_1} > "
-    + timer_file_1)
-
-# second
-jube_id_2 = str(sys.argv[2])
-path_2 = os.path.join(config['jube_outpath'], jube_id_2.zfill(6))
-timer_file_2=os.path.join(path_2, "timer_file.csv")
-shell(
-    f"module load JUBE; jube analyse {config['jube_outpath']} --id {jube_id_2};"
-    + f" jube result {config['jube_outpath']} --id {jube_id_2} > "
-    + timer_file_2)
-
-# third
-jube_id_3 = str(sys.argv[3])
-path_3 = os.path.join(config['jube_outpath'], jube_id_3.zfill(6))
-timer_file_3=os.path.join(path_3, "timer_file.csv")
-shell(
-    f"module load JUBE; jube analyse {config['jube_outpath']} --id {jube_id_3};"
-    + f" jube result {config['jube_outpath']} --id {jube_id_3} > "
-    + timer_file_3)
-
-# fourth
-jube_id_4 = str(sys.argv[4])
-path_4 = os.path.join(config['jube_outpath'], jube_id_4.zfill(6))
-timer_file_4=os.path.join(path_4, "timer_file.csv")
-shell(
-    f"module load JUBE; jube analyse {config['jube_outpath']} --id {jube_id_4};"
-    + f" jube result {config['jube_outpath']} --id {jube_id_4} > "
-    + timer_file_4)
+data_paths, timer_files = [], []
+for i in [1, 2, 3, 4]:
+    jube_id_i = str(sys.argv[i])
+    path_i = os.path.join(config['jube_outpath'], jube_id_i.zfill(6))
+    data_paths.append(path_i)
+    timer_file_i = os.path.join(path_i, "timer_file.csv")
+    timer_files.append(timer_file_i)
+    # analyse and save plot raw data to .csv
+    shell(
+        f"module load JUBE; jube analyse {config['jube_outpath']} --id {jube_id_i};"
+        + f" jube result {config['jube_outpath']} --id {jube_id_i} > "
+        + timer_file_i)
 
 # Labels
 try:
-    label_1 = str(sys.argv[5])
-    label_2 = str(sys.argv[6])
-    label_3 = str(sys.argv[7])
-    label_4 = str(sys.argv[8])
+    labels = []
+    for i in [5, 6, 7, 8]:
+        labels.append(str(sys.argv[i]))
 except:
-    label_1 = "Sparse"
-    label_2 = "Synchronous"
-    label_3 = "Surrogate"
-    label_4 = "No tripartite"
+    labels = ['Sparse', 'Synchronous', 'Surrogate', 'No Tripartite']
 
 # X axis: "nodes" or "nvp"
 # This is for the x-axis labeling: "Number of nodes" or "Number of threads"
@@ -73,9 +46,9 @@ except:
     x_axis_label = "num_nodes"
 
 # take the job and cpu info from first bench job, assuming all nodes are equal
-bench_path = glob.glob(os.path.join(path_1, '*_bench/work'))
+bench_path = glob.glob(os.path.join(data_paths[0], '*_bench/work'))
 bench_path.sort()
-cpu_info = load(os.path.join(bench_path[0], 'cpu.json'))
+#cpu_info = load(os.path.join(bench_path[0], 'cpu.json'))
 job_info = load(os.path.join(bench_path[0], 'job.json'))
 
 # Strong or weak scaling (if not given, get it from job_info)
@@ -85,6 +58,7 @@ try:
 except:
     strength = 'strong'
 
+# Plot layout
 if x_axis_label == "num_nvp":
     cons_ylims = (-0.25, 5.25)
     prop_ylims = (-5.0, 105.0)
@@ -133,36 +107,36 @@ if all_auto:
     spk_ylims = (0, None)
 
 plot_all_quad(
-    timer_file_1=timer_file_1,
-    timer_file_2=timer_file_2,
-    timer_file_3=timer_file_3,
-    timer_file_4=timer_file_4,
-    save_path=path_1,
+    timer_file_1=timer_files[0],
+    timer_file_2=timer_files[1],
+    timer_file_3=timer_files[2],
+    timer_file_4=timer_files[3],
+    save_path=data_paths[0],
     scaling_strength=strength,
     x_axis=x_axis_label,
     cons_ylims = cons_ylims,
     prop_ylims = prop_ylims,
     conn_ylims=conn_ylims,
     spk_ylims = spk_ylims,
-    label_1=label_1,
-    label_2=label_2,
-    label_3=label_3,
-    label_4=label_4,
+    label_1=labels[0],
+    label_2=labels[1],
+    label_3=labels[2],
+    label_4=labels[3],
     conn_plotted=conn_plotted,
 )
 
 plot_phases_quad(
-    timer_file_1=timer_file_1,
-    timer_file_2=timer_file_2,
-    timer_file_3=timer_file_3,
-    timer_file_4=timer_file_4,
-    save_path=path_1,
+    timer_file_1=timer_files[0],
+    timer_file_2=timer_files[1],
+    timer_file_3=timer_files[2],
+    timer_file_4=timer_files[3],
+    save_path=data_paths[0],
     scaling_strength=strength,
     x_axis=x_axis_label,
     rtf_ylims=rtf_ylims,
-    label_1=label_1,
-    label_2=label_2,
-    label_3=label_3,
-    label_4=label_4,
+    label_1=labels[0],
+    label_2=labels[1],
+    label_3=labels[2],
+    label_4=labels[3],
     secondary=False,
 )
