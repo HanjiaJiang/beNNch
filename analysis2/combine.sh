56ab573c5bdd7eb905ec21704fbaaa0f8d67458b
jiang4 <jiang4@jrlogin04.jureca>
2024-10-01 17:14:06 +0200

    Change outpath to production1 for combine.sh

--- analysis2/combine.sh
+++ analysis2/combine.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dpath=../outpath/production/0000$1
+dpath=../outpath/production1/0000$1
 
 convert $dpath/plot_major.png $dpath/legend_major.png +append $dpath/combine_major.png
 convert $dpath/plot_phases.png $dpath/legend_phases.png +append $dpath/combine_phases.png
