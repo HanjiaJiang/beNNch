edbf14512f4491778f6557d54c4464af9043571c
jiang4 <jiang4@jrlogin02.jureca>
2024-11-13 13:17:36 +0100

    Remove redundancy in analysis.py

--- analysis2/analysis.py
+++ analysis2/analysis.py
@@ -1,16 +1,9 @@
 # This script generate the single timer file for plotting and needs JUBE
 import os
 import sys
-import glob
 import yaml
-import csv
-
-import pandas as pd
 
 from analysis_helper import shell, load
-
-import matplotlib.pyplot as plt
-plt.rcParams.update({'font.size': 16})
 
 # Load analysis configurations
 config_file_name = '../config/analysis_config.yaml'
@@ -19,7 +12,7 @@ with open(config_file_name) as analysis_config_file:
 
 # Load data
 data_paths, timer_files = [], []
-for i in [1, 2, 3, 4]:
+for i in range(1, len(sys.argv)):
     jube_id_i = str(sys.argv[i])
     path_i = os.path.join(config['jube_outpath'], jube_id_i.zfill(6))
     data_paths.append(path_i)
