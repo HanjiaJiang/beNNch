e0a044040cdcf27bc144456b54addd55c93375a5
HanjiaJiang <h.jiang@fz-juelich.de>
2024-11-15 15:59:44 +0100

    Simplify plot.py and keep only necessary items

--- analysis/plot.py
+++ analysis/plot.py
@@ -5,8 +5,6 @@ import glob
 import yaml
 import csv
 
-import pandas as pd
-
 from analysis_helper import load
 from plot_phases import plot_phases
 from plot_major import plot_major, plot_conn_fr
@@ -37,54 +35,23 @@ try:
 except:
     labels = ['Sparse', 'Synchronous', 'Surrogate', 'No Tripartite']
 
-# X axis: "nodes" or "nvp"
-# This is for the x-axis labeling: "Number of nodes" or "Number of threads"
-# Use "nvp" for "Number of threads" (nvp strong scaling case)
-try:
-    x_axis_label = "num_nvp" if str(sys.argv[9]) == "nvp" else "num_nodes"
-except:
-    x_axis_label = "num_nodes"
-
-# take the job and cpu info from first bench job, assuming all nodes are equal
+# Strong or weak scaling (if not given, get it from job_info)
+# This is for the "Network size" labeling for weak scaling cases
 bench_path = glob.glob(os.path.join(data_paths[0], '*_bench/work'))
 bench_path.sort()
 job_info = load(os.path.join(bench_path[0], 'job.json'))
-
-# Strong or weak scaling (if not given, get it from job_info)
-# This is for the "Network size" labeling for weak scaling cases
 try:
     strength = job_info['scaling_type']
 except:
     strength = 'strong'
 
 # ylims
-if x_axis_label == "num_nvp":
-    cons_ylims = (-0.01, 0.11)
-    if "Fixed-outdegree" in labels:
-        conn_ylims = (-0.1, 2.1)
-    else:
-        conn_ylims = (-0.02, 0.52)
-    rtf_ylims = (-0.5, 10.5)
-elif strength == "strong":
-    cons_ylims = (-0.01, 0.11)
-    if "Fixed-outdegree" in labels:
-        conn_ylims = (-0.1, 2.1)
-    else:
-        conn_ylims = (-0.02, 0.52)
-    prop_ylims = (-1, 41)
+if strength == "strong":
     rtf_ylims = (-0.1, 2.6)
 else:
-    cons_ylims = (-0.01, 0.11)
-    if "Fixed-outdegree" in labels:
-        conn_ylims = (-0.1, 4.6)
-    else:
-#        conn_ylims = (-0.02, 0.72)
-        conn_ylims = (-0.02, 0.52)
     if "Sparse" in labels or "Bernoulli" in labels:
-        prop_ylims = (-1, 41)
         rtf_ylims = (-0.1, 3.6)
     else:
-        prop_ylims = (-1, 61)
         rtf_ylims = (-0.1, 6.1)
 
 # plot major data
@@ -93,21 +60,9 @@ plot_major(
     labels,
     data_paths[0],
     strength,
-    x_axis=x_axis_label,
-#    cons_ylims=cons_ylims,
-#    conn_ylims=conn_ylims,
-#    prop_ylims=prop_ylims,
     colors=['#004488','#994455','#997700','#6699cc'],
     styles=['-', '--', ':', ':'],
     lw=3,
-)
-
-# plot connection number and firing rate
-plot_conn_fr(
-    timer_files,
-    data_paths[0],
-    strength,
-    x_axis=x_axis_label,
 )
 
 # plot phases
@@ -117,7 +72,6 @@ for (detail, fontsize) in [(False, 'small'), (True, 'x-small')]:
         labels,
         data_paths[0],
         strength,
-        x_axis=x_axis_label,
         rtf_ylims=rtf_ylims,
         detail=detail,
         ignore_others=False,
@@ -130,61 +84,9 @@ plot_separate(
      labels,
      data_paths[0],
      strength,
-     quantities=['time_update_factor', 'spike_ccd_factor', 'secondary_gd_factor', 'others_factor'],
+     ['time_update_factor', 'spike_ccd_factor', 'secondary_gd_factor', 'others_factor'],
+     colors=['#004488','#994455','#997700','#6699cc'],
      file_postfix='rtf',
      ylabel_prefix='RTF of ',
     )
 
-# plot memory data
-plot_separate(
-     timer_files,
-     labels,
-     data_paths[0],
-     strength,
-     quantities=['base_memory', 'network_memory', 'init_memory', 'total_memory'],
-     file_postfix='memory',
-    )
-
-# plot memory difference data
-plot_separate(
-     timer_files,
-     labels,
-     data_paths[0],
-     strength,
-     quantities=['memory_network_minus_base', 'memory_init_minus_network', 'memory_total_minus_init'],
-     file_postfix='memory-diff',
-    )
-
-
-def pivot_df(df, columns, index, values):
-    df_mean = df.groupby([columns, index]).mean().reset_index()
-    df_std = df.groupby([columns, index]).std().reset_index()
-    df_mean_table = df_mean.pivot(columns=columns, index=index, values=values)
-    df_std_table = df_std.pivot(columns=columns, index=index, values=values)
-    df_mean_table.to_csv(f'{values}_mean.csv')
-    df_std_table.to_csv(f'{values}_std.csv')
-
-
-# concatenate and save results
-def concat_df(file_list, output_file, labels_list, compare_memory=False):
-    df_all = None
-    for i, file_name in enumerate(file_list):
-        if not os.path.isfile(file_name):
-            break
-        df = pd.read_csv(file_name)
-        df["model"] = labels_list[i]
-        if df_all is None:
-            df_all = df.copy()
-        else:
-            df_all = pd.concat((df_all, df))
-    df_all.to_csv(output_csv, index=False)
-    if compare_memory and 'network_memory' in df_all:
-        df_all['memory_network_minus_base'] = df_all['network_memory'] - df_all['base_memory']
-        df_all['memory_init_minus_network'] = df_all['init_memory'] - df_all['network_memory']
-        df_all['memory_total_minus_init'] = df_all['total_memory'] - df_all['init_memory']
-        pivot_df(df_all, 'num_nodes', 'model', 'memory_network_minus_base')
-        pivot_df(df_all, 'num_nodes', 'model', 'memory_init_minus_network')
-        pivot_df(df_all, 'num_nodes', 'model', 'memory_total_minus_init')
-
-output_csv = os.path.join(data_paths[0], "df_all.csv")
-concat_df(timer_files, output_csv, labels)
