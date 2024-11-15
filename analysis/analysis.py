ba1e206ae269b3e414a13e083306175831a6b04a
jiang4 <jiang4@jrlogin06.jureca>
2024-11-15 15:26:03 +0100

    Reorganize analysis.py

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -3,27 +3,40 @@ import os
 import sys
 import yaml
 
+import pandas as pd
+
 from analysis_helper import shell, load
 
-# Load analysis configurations
-config_file_name = '../config/analysis_config.yaml'
-with open(config_file_name) as analysis_config_file:
-    config = yaml.load(analysis_config_file, Loader=yaml.FullLoader)
+def analyze(jube_ids, config_file_name):
+    # load analysis configurations
+    with open(config_file_name) as analysis_config_file:
+        config = yaml.load(analysis_config_file, Loader=yaml.FullLoader)
+    data_paths, df_all = [], None
+    # iterate through benchmarks
+    for i in range(len(jube_ids)):
+        jube_id_i = str(jube_ids[i])
+        path_i = os.path.join(config['jube_outpath'], jube_id_i.zfill(6))
+        data_paths.append(path_i)
+        timer_file_i = os.path.join(path_i, "timer_file.csv")
+        # analyse and save plot raw data to .csv
+        if not os.path.isfile(timer_file_i):
+            print(f"reload {timer_file_i} ...")
+            shell(
+                f"module load JUBE; jube analyse {config['jube_outpath']} --id {jube_id_i};"
+                + f" jube result {config['jube_outpath']} --id {jube_id_i} > "
+                + timer_file_i)
+        # collect raw data of this benchmark
+        if os.path.isfile(timer_file_i):
+            df = pd.read_csv(timer_file_i)
+            df["model"] = jube_id_i
+            if df_all is None:
+                df_all = df.copy()
+            else:
+                df_all = pd.concat((df_all, df))
+    # save raw data of all benchmarks
+    df_all.to_csv(os.path.join(data_paths[0], 'df_all.csv'), index=False)
 
-# Load data
-data_paths, timer_files = [], []
-for i in range(1, len(sys.argv)):
-    jube_id_i = str(sys.argv[i])
-    path_i = os.path.join(config['jube_outpath'], jube_id_i.zfill(6))
-    data_paths.append(path_i)
-    timer_file_i = os.path.join(path_i, "timer_file.csv")
-    timer_files.append(timer_file_i)
-    # analyse and save plot raw data to .csv
-    if not os.path.isfile(timer_file_i):
-        print(f"reload {timer_file_i} ...")
-        shell(
-            f"module load JUBE; jube analyse {config['jube_outpath']} --id {jube_id_i};"
-            + f" jube result {config['jube_outpath']} --id {jube_id_i} > "
-            + timer_file_i)
-
-
+if __name__ == "__main__":
+    # JUBE benchmark IDs
+    jube_ids = sys.argv[1:]
+    analyze(jube_ids, '../config/analysis_config.yaml')
