64d7d38dc6c5acc16a03a704c9de81f26dae0689
jiang4 <jiang4@jrlogin09.jureca>
2024-11-20 16:55:38 +0100

    Change run_benchmark_models.sh for user input of core/thread numbers

--- models/astrocyte/run_benchmark_models.sh
+++ models/astrocyte/run_benchmark_models.sh
@@ -5,7 +5,7 @@ declare -a arr=("Sparse" "Synchronous")
 for x in "${arr[@]}"
 do
   echo "Running $x model ..."
-  python run_benchmark_model.py "$x"
+  python run_benchmark_model.py "$x" $1
 done
 
 python plots.py
