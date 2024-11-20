83cb5db63306f945f35f1db16221b088e5488207
HanjiaJiang <h.jiang@fz-juelich.de>
2024-11-20 10:43:42 +0100

    Reorganize and simplify bennchplot.py

--- analysis/bennchplot.py
+++ analysis/bennchplot.py
@@ -18,17 +18,13 @@ SPDX-License-Identifier: GPL-3.0-or-later
 """
 Class for benchmarking plots
 """
+import os
+
 import pandas as pd
 import matplotlib
-from matplotlib import pyplot as plt
-from matplotlib import gridspec
 import numpy as np
-import yaml
-import os
-try:
-    from . import plot_params as pp
-except ImportError:
-    import plot_params as pp
+
+import plot_params as pp
 
 
 class Plot():
@@ -56,9 +52,7 @@ class Plot():
                  data_file='/path/to/data',
                  color_params=pp.color_params,
                  label_params=pp.label_params,
-                 time_scaling=1,
-                 ctrl_file=None,
-                 construct_timer="Python"):
+                 time_scaling=1):
 
         self.x_axis = x_axis
         self.x_ticks = x_ticks
@@ -66,17 +60,11 @@ class Plot():
         self.label_params = label_params
         self.time_scaling = time_scaling
         self.df_data = None
-        self.df_ctrl = None
-        self.construct_timer = construct_timer
 
         self.load_data(data_file)
-        if isinstance(ctrl_file, str):
-            self.load_data(ctrl_file, control=True)
         self.compute_derived_quantities()
-        if isinstance(ctrl_file, str):
-            self.compute_derived_quantities(control=True)
 
-    def load_data(self, data_file, control=False):
+    def load_data(self, data_file):
         """
         Load data to dataframe, to be used later when plotting.
 
@@ -92,12 +80,10 @@ class Plot():
         ValueError
         """
         print(f'Loading {data_file} ...')
-        try:
-            df = pd.read_csv(data_file, delimiter=',')
-        except FileNotFoundError:
-            print('File could not be found')
-            quit()
+        assert os.path.isfile(data_file), 'File could not be found'
+        df = pd.read_csv(data_file, delimiter=',')
 
+        # Set items for major timer data
         for py_timer in ['py_time_create', 'py_time_connect']:
             if py_timer not in df:
                 df[py_timer] = np.nan
@@ -121,7 +107,7 @@ class Plot():
         col = ['num_nodes', 'threads_per_task', 'tasks_per_node',
                'model_time_sim', 'time_construction_create',
                'time_construction_create_std', 'time_construction_connect',
-               'time_construction_connect', 'time_simulate',
+               'time_construction_connect_std', 'time_simulate',
                'time_simulate_std',
                'py_time_create', 'py_time_create_std',
                'py_time_connect', 'py_time_connect_std',
@@ -130,13 +116,14 @@ class Plot():
                'local_spike_counter', 'local_spike_counter_std',
                ]
 
+        # Set items for numbers of cells
         N_recorders = ['N_ex', 'N_in', 'N_astro']
         for N_recorder in N_recorders:
             if N_recorder in df.columns:
                 dict_.update({N_recorder: 'first'})
                 col.append(N_recorder)
 
-        # Timers for connection building in TripartiteConnect()
+        # Set items for detailed timer data
         detailed_timers = [
             'time_update',
             'time_collocate_spike_data',
@@ -160,54 +147,45 @@ class Plot():
                 col.append(timer)
                 col.append(timer+'_std')
 
+        # Use the raw data to calculate group means and standard deviations
         df = df.drop('rng_seed', axis=1).groupby(
             ['num_nodes',
              'threads_per_task',
              'tasks_per_node',
              'model_time_sim'], as_index=False).agg(dict_)
-        df.columns = col
-        if control:
-            self.df_ctrl = df.copy()
-        else:
-            self.df_data = df.copy()
 
-    def compute_derived_quantities(self, control=False):
+        # Keep data in object
+        df.columns = col
+        self.df_data = df
+
+    def compute_derived_quantities(self):
         """
         Do computations to get parameters needed for plotting.
         """
-        if control:
-            df = self.df_ctrl
-        else:
-            df = self.df_data
-        df['num_nvp'] = (
-            df['threads_per_task'] * df['tasks_per_node']
-        )
+        df = self.df_data
+        df['num_nvp'] = (df['threads_per_task'] * df['tasks_per_node'])
         df['model_time_sim'] /= self.time_scaling
-        if self.construct_timer == "C++":
-            df['time_construct'] = (df['time_construction_create'] + df['time_construction_connect'])
-            df['time_construct_std'] = (np.sqrt((df['time_construction_create_std']**2 + df['time_construction_connect_std']**2)))
-        else:
-            df['time_construct'] = (df['py_time_create'] + df['py_time_connect'])
-            df['time_construct_std'] = (np.sqrt((df['py_time_create_std']**2 + df['py_time_connect_std']**2)))
         df['sim_factor'] = (df['time_simulate'] / df['model_time_sim'])
         df['sim_factor_std'] = (df['time_simulate_std'] / df['model_time_sim'])
 
+        # Item that adds up all known phases
+        # This is for calculating the 'other' phase
         df['time_addup'] = (
             df['time_update'] +
+            df['time_collocate_spike_data'] +
             df['time_communicate_spike_data'] +
             df['time_deliver_spike_data'] +
-            df['time_collocate_spike_data'] +
-            df['time_deliver_secondary_data'] +
-            df['time_gather_secondary_data']
+            df['time_gather_secondary_data'] +
+            df['time_deliver_secondary_data']
         )
         df['time_addup_std'] = \
             np.sqrt(
             df['time_update_std']**2 +
+            df['time_collocate_spike_data_std']**2 +
             df['time_communicate_spike_data_std']**2 +
             df['time_deliver_spike_data_std']**2 +
-            df['time_collocate_spike_data_std']**2 +
-            df['time_deliver_secondary_data_std']**2 +
-            df['time_gather_secondary_data_std']**2
+            df['time_gather_secondary_data_std']**2 +
+            df['time_deliver_secondary_data_std']**2
         )
         df['time_addup_factor'] = (
             df['time_addup'] /
@@ -216,54 +194,54 @@ class Plot():
             df['time_addup_std'] /
             df['model_time_sim'])
 
-        # calculate phases and their fractions, using model_time_sim (model time) and time_simulate (wall time)
+        # Calculate phases and their fractions
+        # model_time_sim (model time) and time_simulate (wall time) are needed
         phases = [
             'time_update',
+            'time_collocate_spike_data',
             'time_communicate_spike_data',
             'time_deliver_spike_data',
-            'time_collocate_spike_data',
-            'time_deliver_secondary_data',
             'time_gather_secondary_data',
+            'time_deliver_secondary_data',
         ]
         for phase in phases:
-            df[phase + '_factor'] = (df[phase] / df['model_time_sim'])
-            df[phase + '_frac'] = (100 * df[phase] / df['time_simulate'])
+            df[phase + '_factor'] = df[phase] / df['model_time_sim']
+            df[phase + '_frac'] = 100 * df[phase] / df['time_simulate']
 
         # spike CCD = collocate + communicate + deliver
         df['spike_ccd_factor'] = (
+            df['time_collocate_spike_data_factor'] +
             df['time_communicate_spike_data_factor'] +
-            df['time_deliver_spike_data_factor'] +
-            df['time_collocate_spike_data_factor'])
+            df['time_deliver_spike_data_factor']
+        )
         df['spike_ccd_frac'] = (
+            df['time_collocate_spike_data_frac'] +
             df['time_communicate_spike_data_frac'] +
-            df['time_deliver_spike_data_frac'] +
-            df['time_collocate_spike_data_frac'])
+            df['time_deliver_spike_data_frac']
+        )
 
-        # secondary event (gather and deliver)
+        # secondary GD = gather + deliver
         df['secondary_gd_factor'] = (
-            df['time_deliver_secondary_data_factor'] +
-            df['time_gather_secondary_data_factor'])
+            df['time_gather_secondary_data_factor'] +
+            df['time_deliver_secondary_data_factor']
+        )
         df['secondary_gd_frac'] = (
-            df['time_deliver_secondary_data_frac'] +
-            df['time_gather_secondary_data_frac']
+            df['time_gather_secondary_data_frac'] +
+            df['time_deliver_secondary_data_frac']
         )
 
         # others = the rest
-        df['others_factor'] = (df['time_simulate'] - df['time_addup'])/df['model_time_sim']
+        df['others_factor'] = (df['time_simulate'] - df['time_addup']) / df['model_time_sim']
         df['others_frac'] = 100 * (df['time_simulate'] - df['time_addup']) / df['time_simulate']
 
-        # total spike count
+        # total spike count per second
         df['total_spike_count_per_s'] = (df['local_spike_counter'] / df['model_time_sim'])
         df['total_spike_count_per_s_std'] = (df['local_spike_counter_std'] / df['model_time_sim'])
 
         # average firing rate
         if 'N_ex' in df.columns and 'N_in' in df.columns:
-            df['average_firing_rate'] = df['total_spike_count_per_s']/(df['N_ex'] + df['N_in'])
-            df['average_firing_rate_std'] = df['total_spike_count_per_s_std']/(df['N_ex'] + df['N_in'])
-        else:
-            print('N_ex or N_in not in df!')
-            df['average_firing_rate'] = df['total_spike_count_per_s']/((df['network_size']-1)/2) # minus 1 Poisson generator
-            df['average_firing_rate_std'] = df['total_spike_count_per_s_std']/((df['network_size']-1)/2)
+            df['average_firing_rate'] = df['total_spike_count_per_s'] / (df['N_ex'] + df['N_in'])
+            df['average_firing_rate_std'] = df['total_spike_count_per_s_std'] / (df['N_ex'] + df['N_in'])
 
         if 'base_memory' in df and 'network_memory' in df and 'init_memory' in df and 'total_memory' in df:
             df['memory_network_minus_base'] = df['network_memory'] - df['base_memory']
@@ -271,8 +249,8 @@ class Plot():
             df['memory_total_minus_init'] = df['total_memory'] - df['init_memory']
 
     def plot_fractions(self, axis, fill_variables,
-                       interpolate=False, step=None, log=False, alpha=1.,
-                       error=False, control=False, line=False, subject=None, ylims=None):
+                       interpolate=False, step=None, alpha=1.,
+                       error=False):
         """
         Fill area between curves.
 
@@ -284,40 +262,27 @@ class Plot():
             whether to interpolate between the curves
         step : {'pre', 'post', 'mid'}, optional
             should the filling be a step function
-        log : bool, default
-            whether the x-axes should have logarithmic scale
         alpha, int, default
             alpha value of fill_between plot
         error : bool
             whether plot should have error bars
         """
-        if control:
-            df = self.df_ctrl
-        else:
-            df = self.df_data
+        df = self.df_data
 
         fill_height = 0
         for i, fill in enumerate(fill_variables):
-            main_label = subject if isinstance(subject, str) else None
-            main_label = main_label if fill == fill_variables[-1] else None
-            line_color = 'dimgray' if control else 'k'
-            try:
-                frac_label = self.label_params[fill]
-                if isinstance(subject, str):
-                    frac_label += f' ({subject})'
-                axis.fill_between(np.squeeze(df[self.x_axis]),
-                                  fill_height,
-                                  np.squeeze(df[fill]) + fill_height,
-                                  label=frac_label,
-                                  facecolor=self.color_params[fill],
-                                  interpolate=interpolate,
-                                  step=step,
-                                  alpha=alpha,
-                                  linewidth=0.5,
-                                  edgecolor='#444444')
-            except:
-                print("fill_between() failed!")
-            if error:
+            line_color = 'k'
+            axis.fill_between(np.squeeze(df[self.x_axis]),
+                              fill_height,
+                              np.squeeze(df[fill]) + fill_height,
+                              facecolor=self.color_params[fill],
+                              interpolate=interpolate,
+                              step=step,
+                              alpha=alpha,
+                              linewidth=0.5,
+                              edgecolor='#444444')
+            # for error bars
+            if error and fill + '_std' in df:
                 axis.errorbar(np.squeeze(df[self.x_axis]),
                               np.squeeze(df[fill]) + fill_height,
                               yerr=np.squeeze(df[fill + '_std']),
@@ -325,10 +290,10 @@ class Plot():
                               capthick=1,
                               color=line_color,
                               fmt='none',
-                              label=main_label
                               )
             fill_height += df[fill].to_numpy()
 
+        # Set xticks according to data or user input
         if self.x_ticks == 'data':
             try:
                 axis.set_xticks(np.squeeze(df[self.x_axis]))
@@ -337,18 +302,9 @@ class Plot():
         else:
             axis.set_xticks(self.x_ticks)
 
-        if isinstance(ylims, tuple):
-            axis.set_ylim(ylims)
-
-        if log:
-            axis.set_xscale('log')
-            axis.tick_params(bottom=False, which='minor')
-            axis.get_xaxis().set_major_formatter(
-                matplotlib.ticker.ScalarFormatter())
-
-    def plot_main(self, quantities, axis, log=(False, False),
-                  error_only=False, fmt='none', control=False, subject=None, line_color=None, line_style=None, ylims=None, linewidth=1.5,
-                  alpha=1.0, elinewidth=1.5
+    def plot_main(self, quantities, axis,
+                  line_color=None, line_style=None, linewidth=3,
+                  markersize=10.0, alpha=1.0
                   ):
         """
         Main plotting function.
@@ -359,53 +315,35 @@ class Plot():
             list with plotting quantities
         axis : axis object
             axis object used when plotting
-        log : tuple of bools, default
-            whether x and y axis should have logarithmic scale
         error : bool, default
             whether or not to plot error bars
-        fmt : string
-            matplotlib format string (fmt) for defining line style
         """
-        if control:
-            df = self.df_ctrl
-        else:
-            df = self.df_data
+        df = self.df_data
 
         for i, y in enumerate(quantities):
             if y not in df:
                 continue
-            label = subject if isinstance(subject, str) else f'quantity {i}'
-            if not error_only:
-                axis.plot(df[self.x_axis],
-                          df[y],
-                          marker='.',
-                          markersize=10.0,
-                          color=line_color,
-                          linewidth=linewidth,
-                          linestyle=line_style,
-                          label=label,
-                          alpha=alpha)
-            if y + '_std' in df:
+            axis.plot(df[self.x_axis].values,
+                      df[y].values,
+                      marker='.',
+                      markersize=markersize,
+                      color=line_color,
+                      linewidth=linewidth,
+                      linestyle=line_style,
+                      alpha=alpha)
+            # for error bars
+            str_std = y + '_std'
+            if str_std in df:
                 axis.errorbar(
                     df[self.x_axis].values,
                     df[y].values,
                     yerr=df[y + '_std'].values,
                     marker=None,
-                    capsize=3,
-                    capthick=1.5,
-                    linewidth=elinewidth,
                     color=line_color,
-                    fmt=fmt,
+                    linewidth=0.5*linewidth,
+                    linestyle='none',  # line is not needed in this case
+                    capsize=linewidth,
+                    capthick=0.5*linewidth,
                     alpha=alpha)
-            else:
-                str_std = y + '_std'
+            else:                
                 print(f'plot_main(): {str_std} not in data!')
-
-        if isinstance(ylims, tuple):
-            axis.set_ylim(ylims)
-
-        if log[0]:
-            axis.set_xscale('log')
-        if log[1]:
-            axis.tick_params(bottom=False, which='minor')
-            axis.set_yscale('log')
