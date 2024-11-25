3d37b5ba592dd25c7c5a18cb32b71aeea0cc908e
HanjiaJiang <h.jiang@fz-juelich.de>
2024-11-25 16:24:29 +0100

    Fix folder moving problem in run_analysis.sh

--- analysis/run_analysis.sh
+++ analysis/run_analysis.sh
@@ -21,8 +21,13 @@ python plot.py 4 10 11 12 Bernoulli "Fixed in-degree" "Fixed out-degree" "Fixed 
 python plot.py 4 16 17 18 pool_size=10 pool_size=100 pool_size=1000 pool_size=10000
 
 # Change result folder names
-mv results_Sparse_Synchronous_Surrogate_x_strong results_fig6a
-mv results_Sparse_Synchronous_Surrogate_x_weak results_fig6b
-mv results_Bernoulli_Fixedindegree_Fixedoutdegree_Fixedtotalnumber_strong results_fig7a
-mv results_Bernoulli_Fixedindegree_Fixedoutdegree_Fixedtotalnumber_weak results_fig7b
-mv results_poolsize=10_poolsize=100_poolsize=1000_poolsize=10000_weak results_fig8
+rsync -au results_Sparse_Synchronous_Surrogate_x_strong/ results_fig6a
+rsync -au results_Sparse_Synchronous_Surrogate_x_weak/ results_fig6b
+rsync -au results_Bernoulli_Fixedindegree_Fixedoutdegree_Fixedtotalnumber_strong/ results_fig7a
+rsync -au results_Bernoulli_Fixedindegree_Fixedoutdegree_Fixedtotalnumber_weak/ results_fig7b
+rsync -au results_poolsize=10_poolsize=100_poolsize=1000_poolsize=10000_weak/ results_fig8
+rm -r results_Sparse_Synchronous_Surrogate_x_strong
+rm -r results_Sparse_Synchronous_Surrogate_x_weak
+rm -r results_Bernoulli_Fixedindegree_Fixedoutdegree_Fixedtotalnumber_strong
+rm -r results_Bernoulli_Fixedindegree_Fixedoutdegree_Fixedtotalnumber_weak
+rm -r results_poolsize=10_poolsize=100_poolsize=1000_poolsize=10000_weak
